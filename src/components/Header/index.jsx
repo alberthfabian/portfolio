d95d167e4af46f45ba9007ebf0b8c8401ca5b86b
Alberth Perez <alberth.perez@hotmail.com>
2020-11-02 21:32:13 -0500

    Add Images

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -15,45 +15,6 @@ const Header = () => {
       </article>
       <Menu/>
       <Navbar/>
-      {/* <Nav>
-        <Ul>
-          <Li>
-            <StyledLink to='/'>
-              <P>Home</P> 
-            </StyledLink>
-          </Li>
-          <Li>
-            <StyledLink to='/about'>
-              <P>About</P> 
-            </StyledLink>
-          </Li>
-          <Li>
-            <StyledLink to='/projects'>
-              <P>Projects</P> 
-            </StyledLink>
-          </Li>
-          <Li>
-            <StyledLink to='/skills'>
-              <P>Skills</P> 
-            </StyledLink>
-          </Li>
-          <Li>
-            <StyledLink to='/certificates'>
-              <P>Certificates</P> 
-            </StyledLink>
-          </Li>
-          <Li>
-            <StyledLink to='/blog'>
-              <P>Blog</P> 
-            </StyledLink>
-          </Li>
-          <Li>
-            <StyledLink to='/contact'>
-              <P>Contact</P> 
-            </StyledLink>
-          </Li>
-        </Ul>
-      </Nav> */}
     </Head>
   );
 };
